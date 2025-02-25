03571069a21893de34ee106eba945100ffa09e45
Mark Faust <markfaust123@gmail.com>
2025-02-25 18:05:45 -0500

    added new elements to App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import DocsButton from "./components/buttons/DocsButton";
+import RefreshDatabaseButton from "./components/buttons/RefreshDatabaseButton";
+import ResetDatabaseButton from "./components/buttons/ResetDatabaseButton";
+import FileUpload from "./components/elements/FileUpload";
+import Header from "./components/elements/Header";
+import RagAgentAPI from "./components/elements/RagAgentAPI";
+import SubmitQueryForm from "./components/elements/SubmitQueryForm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <SubmitQueryForm />
+      <RefreshDatabaseButton />
+      <ResetDatabaseButton />
+      <FileUpload />
+      <DocsButton />
+      <RagAgentAPI />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
