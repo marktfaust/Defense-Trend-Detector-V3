c2b6509fc3282e6ee98dc82240af5cbdc8fae7f3
Mark Faust <markfaust123@gmail.com>
2025-02-26 18:23:42 -0500

    cleaned up App.tsx file

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,13 @@
-import DocsButton from "./components/buttons/DocsButton";
-import RefreshDatabaseButton from "./components/buttons/RefreshDatabaseButton";
-import ResetDatabaseButton from "./components/buttons/ResetDatabaseButton";
-import FileUpload from "./components/elements/FileUpload";
 import Header from "./components/elements/Header";
 import RagAgentAPI from "./components/elements/RagAgentAPI";
-import ResponseDetails from "./components/elements/ResponseDetails";
 import SocialFooter from "./components/elements/SocialFooter";
-import SubmitQueryForm from "./components/elements/SubmitQueryForm";
 import RagSection from "./components/sections/RagSection";
 
 const App = () => {
   return (
     <div className="max-w-5xl mx-auto p-4">
       <Header />
-
-      {/* <div className="flex flex-row gap-8">
-        {/* Left Column */}
-        
-      
       <RagSection />
-
-      {/* <ResponseDetails /> */}
       <RagAgentAPI />
       <SocialFooter
         personalWebsite="https://marktfaust.com"
