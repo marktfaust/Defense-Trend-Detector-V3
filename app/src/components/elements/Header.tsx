df36b5c7f95af47e802816ee31e1bb76ccbb4193
Mark Faust <markfaust123@gmail.com>
2025-02-25 19:51:35 -0500

    improved style of header and allowing image display

--- app/src/components/elements/Header.tsx
+++ app/src/components/elements/Header.tsx
@@ -1,20 +1,22 @@
 const Header = () => {
   return (
-    <>
-      <div>
-        <h1>Defense Trend Detector</h1>
-        <a href="https://" target="_blank">
-          <img src={"/trend-svgrepo-com.svg"} className="logo" alt="Trend" />
+    <header className="text-center">
+      <h1 className="text-6xl font-bold mb-8 text-slate-800">
+        Defense Trend Detector
+      </h1>
+      <div className="flex justify-center gap-4">
+        <a href="https://" target="_blank" rel="noopener noreferrer">
+          <img src="/trend-svgrepo-com.svg" alt="Trend" className="w-20 h-20" />
         </a>
-        <a href="https://" target="_blank">
+        <a href="https://" target="_blank" rel="noopener noreferrer">
           <img
-            src={"/fighter-jet-svgrepo-com.svg"}
-            className="logo"
+            src="/fighter-jet-svgrepo-com.svg"
             alt="Jet"
+            className="w-20 h-20"
           />
         </a>
       </div>
-    </>
+    </header>
   );
 };
 
