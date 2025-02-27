98e60912e4074e4c848f7b088811a918b6cc83b0
Mark Faust <markfaust123@gmail.com>
2025-02-26 23:07:56 -0500

    updated component to make use of envionrment variable to detect runtime

--- app/src/components/elements/RagAgentAPI.tsx
+++ app/src/components/elements/RagAgentAPI.tsx
@@ -1,7 +1,12 @@
 const RagAgentAPI = () => {
+  const isProduction = import.meta.env.VITE_PRODUCTION_ENV === "true";
+  const src = isProduction
+    ? "https://defensetrenddetector.thebarrax.co/api/rag_agent/"
+    : "http://127.0.0.1:7860/";
+
   return (
     <iframe
-      src="http://127.0.0.1:7860/"
+      src={src}
       title="Embedded Website"
       width="100%"
       height="600"
